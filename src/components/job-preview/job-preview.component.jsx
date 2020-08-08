010e8a8a7308ab613192b85fae4a255f7d718424
abhu-A-J <abhushanadhikarijoshi@gmail.com>
2020-08-08 13:31:15 +0530

    Completed the main-content display and the jobs preview components

---
+++ src/components/job-preview/job-preview.component.jsx
@@ -0,0 +1,39 @@
+import React from "react";
+
+import "./job-preview.styles.scss";
+
+import globe from "../../asset/iconmonstr-globe-6.svg";
+import clock from "../../asset/iconmonstr-time-3.svg";
+
+const JobPreview = ({ url, name, title, days, location }) => {
+  return (
+    <div className="preview__card">
+      <div className="left-group">
+        <div className="company-logo">
+          <img
+            src="https://jobs.github.com/rails/active_storage/blobs/eyJfcmFpbHMiOnsibWVzc2FnZSI6IkJBaHBBdGFIIiwiZXhwIjpudWxsLCJwdXIiOiJibG9iX2lkIn19--0fa9b48edb8f52d899dfc93962c463442d19ac5c/Square_Logo1.png"
+            alt="Company Logo"
+          />
+        </div>
+
+        <div className="company__details">
+          <p className="company-name">Kasisto</p>
+          <h2 className="title">Front-End Software Engineer</h2>
+          <p class="tag">Full Time</p>
+        </div>
+      </div>
+
+      <div className="right-group">
+        <p class="info">
+          <img src={globe} alt="Globe" />
+          New York
+        </p>
+        <p class="info">
+          <img src={clock} alt="Clock" />5 Days Ago
+        </p>
+      </div>
+    </div>
+  );
+};
+
+export default JobPreview;
--- src/pages/homepage/homepage.component.jsx
+++ src/pages/homepage/homepage.component.jsx
@@ -3,9 +3,22 @@ import React from "react";
 import "./homepage.styles.scss";
 
 import Header from "../../components/header/header.component";
+import JobPreview from "../../components/job-preview/job-preview.component";
 
 const HomePage = () => {
-  return <Header />;
+  return (
+    <>
+      <Header />
+      <div className="main__content u-container">
+        <div className="filter__box"></div>
+        <div className="filtered__result">
+          <JobPreview />
+          <JobPreview />
+          <JobPreview />
+        </div>
+      </div>
+    </>
+  );
 };
 
 export default HomePage;
